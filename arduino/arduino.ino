81d7eaf01ff1873c41d2b5747aa3819be6e5e7ab
jpzg <jacobgrossbard@gmail.com>
2014-02-26 16:56:37 -0500

    Started writing.
    Added more basic stuff to the arduino program and added module import
    for bridgeclient.py on python side

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,15 +1,27 @@
 #include <Bridge.h>
-#include <YunServer.h>
-#include <YunClient.h>
+#include <Mailbox.h>
+#include <Servo.h>
 
 // Arduino-side script for controlling the Yun robot
+// Servo library used to control motors: 0 = full reverse, 90 = stopped, 180 = full forward
+Servo rightmotor;
+Servo leftmotor;
+String msg;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Start bridge,start mailbox, attach motors
+  rightmotor.attach(9);
+  leftmotor.attach(10);
   Bridge.begin();
+  Mailbox.begin();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  
+  if(Mailbox.messageAvailable() != 0){
+    Mailbox.readMessage(msg,Mailbox.messageAvailable());
+  }
+  //if(msg.readStringUntil(':') == 'lm'){
+    //leftmotor.write(int(msg.readString
+  //}
 }
--- python/linino.py
+++ python/linino.py
@@ -1,2 +1,3 @@
-while true:
-	print 'Hello world'
+import imp
+bridge = imp.load_source('bridge','/usr/lib/python2.7/bridge/bridgeclient.py')
+
