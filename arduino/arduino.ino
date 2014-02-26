04b6de16a6a7ce4b13d7cef7edffb82bf44b3288
jpzg <jacobgrossbard@gmail.com>
2014-02-26 00:00:35 -0500

    Created basic files
    Added main arduino program with basic structure and references as well
    as empty python program.

---
+++ arduino/arduino.ino
@@ -0,0 +1,15 @@
+#include <Bridge.h>
+#include <YunServer.h>
+#include <YunClient.h>
+
+// Arduino-side script for controlling the Yun robot
+
+void setup() {
+  // put your setup code here, to run once:
+  Bridge.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
