66944551add239cf4a53c0149d0d73bb891cd68f
jpzg <jacobgrossbard@gmail.com>
2014-02-28 00:40:24 -0500

    Made linino use mailbox.py instead of bridgeclient.py
    Don't know if it will work. Arduino already uses mailbox class.
    Also made some changes I thought I committed before.

--- python/linino.py
+++ python/linino.py
@@ -1,18 +1,17 @@
 import socket
 import imp
-#bridge = imp.load_source('bridge','/usr/lib/python2.7/bridge/bridgeclient.py')
-test = imp.load_source('test','moduletest.py')
-test.greeting();
+mailbox = imp.load_source('bridge','/usr/lib/python2.7/bridge/mailbox.py')
+#test = imp.load_source('test','moduletest.py')
+#test.greeting();
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('localhost', 5005))
 s.listen(1);
 
-conn, addr = s.accept()
-print 'Connection address:',addr
 while True:
-    data = conn.recv(64)
-    if not data: break
-    print "recieved data:",data
-    conn.send("a;lsdjf you")
-conn.close()
+	conn, addr = s.accept()
+	while True:
+		data = conn.recv(64)
+		if not data: break
+		mailbox.send(data)
+	conn.close()
