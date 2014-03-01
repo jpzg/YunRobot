5e865736f72f134d10ff68ac3d7cae871bb7bf45
Jacob Grossbard <jacobgrossbard@gmail.com>
2014-03-01 01:47:55 -0500

    Started keyboard controls for windows app
    also added the socket communications, untested

--- WindowsControlApp/WindowsControlApp/Form1.Designer.cs
+++ WindowsControlApp/WindowsControlApp/Form1.Designer.cs
@@ -29,11 +29,80 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.KeyEvtProvider = new MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Connect";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Not connected to Yun";
+            // 
+            // KeyEvtProvider
+            // 
+            this.KeyEvtProvider.Enabled = false;
+            this.KeyEvtProvider.HookType = MouseKeyboardActivityMonitor.Controls.HookType.Global;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private MouseKeyboardActivityMonitor.Controls.MouseKeyEventProvider KeyEvtProvider;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- WindowsControlApp/WindowsControlApp/Form1.cs
+++ WindowsControlApp/WindowsControlApp/Form1.cs
@@ -7,14 +7,106 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using MouseKeyboardActivityMonitor.WinApi;
 
 namespace WindowsControlApp
 {
     public partial class Form1 : Form
     {
+        public Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        private int lm = 90;
+        private int rm = 90;
+
         public Form1()
         {
             InitializeComponent();
+            KeyEvtProvider.KeyDown += KeyEvtProvider_KeyDown;
+            KeyEvtProvider.KeyUp += KeyEvtProvider_KeyUp;
+            timer1.Interval = 1000 / 60; // Refresh 60 times per second
+            timer1.Tick += timer1_Tick;
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            if (lm < 0) { lm = 0; }
+            if (rm < 0) { rm = 0; }
+            if (lm > 180) { lm = 180; }
+            if (rm > 180) { rm = 180; }
+            label2.Text = String.Format("left motor: {0}", lm);
+            label3.Text = String.Format("right motor: {0}", rm);
+        }
+
+        void KeyEvtProvider_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W || e.KeyCode == Keys.S || e.KeyCode == Keys.Q || e.KeyCode == Keys.E)
+            {
+                lm = 90;
+                rm = 90;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                lm += 20;
+                rm -= 20;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                lm -= 20;
+                rm += 20;
+            }
+        }
+
+        void KeyEvtProvider_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.W){
+                lm = 150;
+                rm = 150;
+            }
+            else if (e.KeyCode == Keys.S)
+            {
+                lm = 50;
+                rm = 50;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                lm -= 20;
+                rm += 20;
+            }
+            else if (e.KeyCode == Keys.D)
+            {
+                lm += 20;
+                rm -= 20;
+            }
+            if(e.KeyCode == Keys.Q || e.KeyCode == Keys.E && (lm != 90 || rm != 90)){
+                e.SuppressKeyPress = true;
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                lm = 60;
+                rm = 120;
+            }
+            else if (e.KeyCode == Keys.E)
+            {
+                lm = 120;
+                rm = 60;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Connecting";
+            //socket.Connect("arduino.local", 3146);
+            //if (socket.Connected)
+            //{
+                label1.Text = "Connected to Yun";
+                button1.Enabled = false;
+                timer1.Enabled = true;
+                KeyEvtProvider.Enabled = true;
+            //}
+            //else
+            //{
+            //    label1.Text = "Could not connect to Yun";
+            //}
         }
     }
 }
