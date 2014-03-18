db4bba2bb8c265209fd22a0453bc545685f4df19
jpzg <jacobgrossbard@gmail.com>
2014-03-17 22:20:51 -0400

    IT WORKS!!!
    *linino.py* Changed socket binding address so it would work when
    connected to network 'condo'. Also changed serial port to the correct
    one and added set baudrate. Exception handling for running commands as
    well.
    *inittab* This is /etc/inittab on the Yun. Just commented the last line
    to prevent it from starting a console on the serial port.

--- python/linino.py
+++ python/linino.py
@@ -2,10 +2,10 @@ import socket
 from pyfirmata import Arduino, util
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('192.168.2.120', 3146))
+s.bind(('192.168.1.9', 3146))
 s.listen(1);
 
-board = Arduino('/dev/tty0')
+board = Arduino('/dev/ttyATH0', baudrate=57600)
 
 while True:
 	conn, addr = s.accept()
@@ -16,6 +16,11 @@ while True:
 		except Exception:
 			break
 		if not data: break
-		exec data
+		try:
+			exec data
+			print "Executed command : " + data + " from " + addr[0]
+		except:
+			print "Error while running command from " + addr[0]
+			print "Command: " + data
 	conn.close()
 	print "Connection closed"
