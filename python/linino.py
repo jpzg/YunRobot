33d2667c4b204532dc4d9c4dd5a3a5911f83e7b3
jpzg <jacobgrossbard@gmail.com>
2014-03-27 23:54:45 -0400

    Started using websockets and Tornado

--- python/linino.py
+++ python/linino.py
@@ -1,41 +1,41 @@
-import socket
 import re
 from pyfirmata import Arduino, util
+from tornado import websocket, web, ioloop
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+cl = []
+class SocketHandler(websocket.WebSocketHandler):
 
-#s.bind(('192.168.240.1', 3146))
-print('Enter ip address for command socket:')
-s.bind((raw_input(),3146))
-s.listen(1);
-print('Socket created.')
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+            print "Client connected:",self
+    def on_message(self, message):
+        print "Message from client: " + message
+    def on_close(self):
+        cl.remove(self)
+        print "Client disconnected:",self
 
-board = Arduino('/dev/ttyATH0', baudrate=115200)
-pin = [None]*14
-p = re.compile(r'\d')
+app = web.Application([(r'/ws', SocketHandler)])
+if __name__ == '__main__':
+    app.listen(3147)
+    ioloop.IOLoop.instance().start()
 
-while True:
-	conn, addr = s.accept()
-	print "Connection accepted",addr
-	while True:
-		try:
-			data = conn.recv(64)
-		except Exception:
-			break
-		if not data: break
-		try:
-			if "board.get_pin" in data: # Handle the get_pin command
-				pin[int(p.findall(data)[0])] = eval(data)
-			else:
-				value = eval(data) # Handle all other commands
-			print "Executed command : " + data + " from " + addr[0]
-			if(value): # Send any output from the command back through the socket
-				conn.send(str(value) + "\n")
-				print "Command returned value: " + str(value)
-				value = None
-		except Exception,e:
-			print "Error while running command from " + addr[0]
-			print e
-	conn.close()
-	print "Connection closed"
+#board = Arduino('/dev/ttyATH0', baudrate=115200)
+#pin = [None] * 14
+#p = re.compile(r'\d')
+
+#while True:
+#	while True:
+#		try:
+#			if "board.get_pin" in data: # Handle the get_pin command
+#				pin[int(p.findall(data)[0])] = eval(data)
+#			else:
+#				value = eval(data) # Handle all other commands
+#			print "Executed command : " + data
+#			if(value): # Send any output from the command back through the socket
+
+#				print "Command returned value: " + str(value)
+#				value = None
+#		except Exception,e:
+#			print "Error while running command."
+#			print e
