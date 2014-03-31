b43b93ca10801aa610987fe0fe0206dd116f88bd
jpzg <jacobgrossbard@gmail.com>
2014-03-30 20:04:15 -0400

    Now completely written for WebSockets
    No longer supports get_pin, if it ever actually did. Haven't tested
    much. Works with writing and (sort of) reading digital pins.

--- python/linino.py
+++ python/linino.py
@@ -1,41 +1,39 @@
-import re
 from pyfirmata import Arduino, util
 from tornado import websocket, web, ioloop
 
+INPUT = 0
+OUTPUT = 1
+ANALOG = 2
+PWM = 3
+SERVO = 4
+
+yun = Arduino('/dev/ttyATH0', baudrate=115200);
+
 cl = []
+pin = [None]*14
 class SocketHandler(websocket.WebSocketHandler):
 
     def open(self):
         if self not in cl:
             cl.append(self)
-            print "Client connected:",self
+            print "[INFO] New connection:", self.request.remote_ip
+            
     def on_message(self, message):
-        print "Message from client: " + message
+        try:
+            print '[CMD]', message
+            value = eval(message)
+            if value:
+                print value
+                self.write_message(str(value))
+                
+        except Exception,e:
+            print '[ERROR]', e
+            
     def on_close(self):
         cl.remove(self)
-        print "Client disconnected:",self
+        print "[INFO] Client disconnected:", self.request.remote_ip
 
 app = web.Application([(r'/ws', SocketHandler)])
 if __name__ == '__main__':
-    app.listen(3147)
+    app.listen(3146)
     ioloop.IOLoop.instance().start()
-
-#board = Arduino('/dev/ttyATH0', baudrate=115200)
-#pin = [None] * 14
-#p = re.compile(r'\d')
-
-#while True:
-#	while True:
-#		try:
-#			if "board.get_pin" in data: # Handle the get_pin command
-#				pin[int(p.findall(data)[0])] = eval(data)
-#			else:
-#				value = eval(data) # Handle all other commands
-#			print "Executed command : " + data
-#			if(value): # Send any output from the command back through the socket
-
-#				print "Command returned value: " + str(value)
-#				value = None
-#		except Exception,e:
-#			print "Error while running command."
-#			print e
