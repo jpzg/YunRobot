f6ee81826efd6190733f38e1fc6138c59a5a342d
jpzg <jacobgrossbard@gmail.com>
2014-04-07 22:43:20 -0400

    get() now works correctly
    Also commented out the script reference to D3 in index.html, wasn't
    currently using it.

--- www/index.js
+++ www/index.js
@@ -1,10 +1,10 @@
-var socket;
-var console;
-var buffer = new Array();
+var socket; // WebSocket object
+var buffer = new Array(); // Buffer for functions to be called on message received
 
-var get = function(s){
-	socket.send(s);
-	return buffer.pop();
+var get = function(msg,f,o){ // Function for executing Yun commands that return a value.
+	socket.send(msg); // o is defaults to document but neccessary for console.log or similar
+	if(!o){ o = document; }
+	buffer.push(function(s){f.call(o,s)});
 }
 var updatePins = function(){
 	$('#digital > tbody tr').each(function(index) {
@@ -27,16 +27,17 @@ var updatePins = function(){
 }
 $(function(){
 	$('#nav-title').text('Robot @ '+ window.location.host);
-	//socket = new WebSocket('ws://' + window.location.host + ':3146');
+	//socket = new WebSocket('ws://' + window.location.host + ':3146/ws');
 	socket = new WebSocket('ws://192.168.2.120:3146/ws'); // Just for testing
 	socket.onopen = function(evt){
 		$('#conn-status').removeClass('alert-warning').addClass('alert-success').text('Connected');
 		updatePins.timer = window.setInterval(updatePins,1000);
 		socket.send('yun.digital[13].write(1)');
-		console.log(get('yun.digital[13].read()'));
+		//get('yun.digital[13].read()',console.log,console);
+		setTimeout(function(){socket.send('yun.digital[13].write(0)')},5000);
 	}
 	socket.onmessage = function(evt){
-		buffer.push(evt.data);
+		buffer.pop().call(document,evt.data); // pops a function off the buffer and runs it with the received value as an arg
 	}
 	socket.onerror = function(evt){
 		$('#conn-status').removeClass('alert-success').addClass('alert-danger').text('Socket Error');
