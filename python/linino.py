0aeec45ac1839afa0c4d1e94dc1c028e70455a0f
jpzg <jacobgrossbard@gmail.com>
2014-04-15 02:20:09 -0400

    Started implementing roles in linino.py
    Also commented code, cleaned it, and started adding an event system for
    messages which are NOT to be immediately executed as Python. These will
    use JSON.

--- python/linino.py
+++ python/linino.py
@@ -1,6 +1,8 @@
 from pyfirmata import Arduino, util
 from tornado import websocket, web, ioloop
+import json
 
+### Values for pin types
 INPUT = 0
 OUTPUT = 1
 ANALOG = 2
@@ -9,30 +11,58 @@ SERVO = 4
 
 yun = Arduino('/dev/ttyATH0', baudrate=115200)
 
-cl = {'drive':None,'servo':None}
-ncl = []
+cl = {'drive':None,'servo':None} # Dict of roles, clients who fill them are put in under that index
+ncl = [] # Array of clients without roles, e.g.  any who are just looking at info.
+def removeClient(client): # Remove client from ncl if present, before looking through roles
+    if client in ncl:
+        ncl.remove(client)
+        return 1
+    else:
+        for k,v in cl.iteritems():
+            if v == client:
+                cl[k] = None
+                return 2,k
+    return -1
 
-pin = [None] * 14
+def broadcast(message): # Broadcast a single message to clients.  Pin updates, role openings, etc.
+    for client in ncl:
+        client.write_message(message)
+    for client in cl.values():
+        if client:
+            client.write_message(message)
+
+### Class to handle websocket connections
 class SocketHandler(websocket.WebSocketHandler):
 
-    def open(self):
+    def open(self): # Add new client to dict of roles or array of role-less clients
         if self not in ncl and self not in cl.values():
             ncl.append(self)
             print "[INFO] New connection:", self.request.remote_ip
             
-    def on_message(self, message):
+    def on_message(self, message): # Execute received message as python code and send back any returned value,
+                                   # unless the '_evt_' prefix is present
         try:
-            print '[CMD]', message
-            value = eval(message)
-            if value != None:
-                print value
-                self.write_message(str(value))
+            if message[0:4] == '_evt_': # Check for evt prefix
+                if 'getRole' in message: # Switch client role
+                    result,r = removeClient(self)
+                    role = message[message.find(':') + 1:]
+                    cl[role] = self
+                    broadcast(json.dumps({'type':'event.closeRole', 'role':role}))
+                    if result == 2:
+                        broadcast(json.dumps({'type':'event.openRole','role':r}))
+                    print '[EVT]', self.request.remote_ip + ' switched roles to ' + role
+            else:
+                print '[CMD]', message, self.request.remote_ip
+                value = eval(message)
+                if value != None:
+                    print value
+                    self.write_message(str(value))
         except Exception,e:
             print '[ERROR]', e
             self.write_message(e)
             
-    def on_close(self):
-        cl.remove(self)
+    def on_close(self): # Remove client
+        removeClient(self)
         print "[INFO] Client disconnected:", self.request.remote_ip
 
 app = web.Application([(r'/ws', SocketHandler)])
