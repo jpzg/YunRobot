79ae2954b80a7ac2a60ba4ede1f73912ac20cc80
jpzg <jacobgrossbard@gmail.com>
2014-10-20 17:01:37 -0400

    Square 2-dimensional slider and js motor classes
    Needs some cleaning

--- www/index.js
+++ www/index.js
@@ -7,6 +7,42 @@ var get = function (msg, f, o) { // Execute any Yun method which returns a value
     if (!o) { o = document; }
     buffer.push(function (s) { f.call(o, s) });
 }
+var Motor = function(port){
+    this.port = port;
+    this._dir = 0;
+    this._spd = 0;
+    this.__defineGetter__("dir",function(){
+        return this._dir;
+    });
+    this.__defineSetter__("dir",function(dir){
+        socket.send('m' + this.port + '.dir =' + dir);
+        this._dir = dir;
+    });
+    this.__defineGetter__("spd",function(){
+        return this._spd;
+    });
+    this.__defineSetter__("spd", function (spd) {
+        spd = (spd < 0) ? 0 : Math.round(spd);
+        if (spd != this._spd) {
+            socket.send('m' + this.port + '.spd = ' + spd);
+            this._spd = spd;
+        }
+    });
+};
+var motor = {
+    p1:new Motor(1),
+    p2:new Motor(2),
+    onMove:function (instance, event, pointer) {
+        motor.p1.spd = instance.position.x;
+        motor.p2.spd = instance.position.y;
+    },
+};
+var motor_onMove = function (instance, event, pointer) {
+    motor.p1.spd = instance.position.x;
+    motor.p2.spd = instance.position.y;
+    $('#display').text('X:' + instance.position.x + '  |  Y:' + instance.position.y);
+}
+var servo = new Object();
 var setServo = function () {
     if (setServo.pos < 0) { setServo.pos = 0;}
     if (setServo.pos != setServo.prev) {
@@ -14,28 +50,14 @@ var setServo = function () {
         setServo.prev = setServo.pos;
     }
 }
-var setMotor = function () {
-    if (setMotor.pos < 0) { setMotor.pos = 0;}
-    if (setMotor.pos != setMotor.prev) {
-        socket.send('m.speed(' + Math.round(setMotor.pos) + ')');
-        setMotor.prev = setMotor.pos;
-    }
-}
-setServo.pos = 0;
-setMotor.pos = 0;
 
 var servo_onMove = function (instance, event, pointer) {
     $('#pos').text(instance.position.x * (160 / 255));
     setServo.pos = instance.position.x * (160 / 255);
 }
-var motor_onMove = function (instance, event, pointer) {
-    $('#pos2').text(instance.position.y);
-    setMotor.pos = instance.position.y;
-}
 var connect = function (ip) {
-    host = ip
-    $('#nav-title').text('Robot @ ' + host);
-    socket = new WebSocket('ws://' + host + ':3146/ws');
+    $('#nav-title').text('Robot @ ' + ip);
+    socket = new WebSocket('ws://' + ip + ':3146/ws');
     socket.onmessage = function (evt) {
         buffer.pop().call(document, evt.data); // pops a function off the buffer and runs it with the received value as an arg
     }
@@ -44,33 +66,29 @@ var connect = function (ip) {
     }
     socket.onclose = function (evt) {
         $('#conn-status').removeClass('alert-success').addClass('alert-warning').text('Socket Closed');
-        window.clearInterval(setServo.interval);
-        window.clearInterval(setMotor.interval);
     }
-    return socket
+    return socket;
 }
 
 $(function () {
 	// Determine robot IP and open socket
     //if (!window.location.host) { host = prompt('What is the robot IP?'); }
     //else { host = window.location.host; }
-    host = '192.168.1.7';
+    host = '192.168.1.254';
 	var socket = connect(host);
     socket.onopen = function (evt) {
         $('#conn-status').removeClass('alert-warning').addClass('alert-success').text('Connected');
-        setServo.interval = window.setInterval(setServo, 25);
-        setMotor.interval = window.setInterval(setMotor, 100);
     }
 
 	// Create draggable object for servo slider and attach movement event handler
-    var servo = new Draggabilly($('#x-slider')[0], {
+    /*servo.drag = new Draggabilly($('#x-slider')[0], {
         axis: 'x',
         containment: '#x-slider-c'
+    });*/
+    motor.drag = new Draggabilly($('#motor-slider')[0], {
+        containment: '#motor-container'
     });
-    var motor = new Draggabilly($('#y-slider')[0], {
-        axis: 'y',
-        containment: '#y-slider-c'
-    });
-    servo.on('dragMove', servo_onMove);
-    motor.on('dragMove', motor_onMove);
+    //servo.on('dragMove', servo_onMove);
+    motor.drag.on('dragMove', motor_onMove);
+    $('')
 });
